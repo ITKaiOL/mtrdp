a232d707d9be2073d4210535e7934afec8d228e8
Kevin Lam @ SP6 <kol.github@bbs2.hk>
2020-07-17 17:14:26 +0800

    First version

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# mtrdp
-Day pass or not?
+# 全日通優惠分析 Day-pass Analyser
+
+全日通: <http://www.mtr.com.hk/ch/customer/tickets/day_pass.html>
+數據來自 data.gov.hk: <https://data.gov.hk/tc-data/dataset/mtr-data-routes-fares-barrier-free-facilities>
+
+- 八折優惠 <http://www.mtr.com.hk/ch/customer/main/fare-relief.html>
+- 早鳥優惠 <http://www.mtr.com.hk/ch/customer/main/early_bird.html>
+- 建議尖沙咀站/尖東轉乘
+- 建議最佳全日通路線使用範圍
+
+計算結果只作參考，或與實際不同。
+
+---
+
+Day pass: <http://www.mtr.com.hk/en/customer/tickets/day_pass.html>
+Using data from data.gov.hk: <https://data.gov.hk/tc-data/dataset/mtr-data-routes-fares-barrier-free-facilities>
+
+- 20% discount <http://www.mtr.com.hk/en/customer/main/fare-relief.html>
+- Early-bird discount <http://www.mtr.com.hk/en/customer/main/early_bird.html>
+- TST / E.TST interchange suggestion
+- Day pass range suggestion
+
+For reference only. Fares calculated may not be accurate.
---
+++ mtrdp.js
@@ -0,0 +1,144 @@
+// Core App object
+window.MTRDP = {
+  config: {},        // configuarations
+  languages: [],     // language options
+  strings: {},       // string literals
+  names: {},         // names (lines and stations)
+  lines: {},         // lines and stations  
+  fares: [],         // fares between two stations
+  fareDistances: [], // distance between two stations by minimum travelling fares
+  minFare: Infinity, // minimum fare
+  sections: {},      // available sections
+};
+
+(function(app) {
+  
+  var scripts = {};
+  
+  // import JS/CSS file
+  // async
+  app.load = function(url) {
+    if(!scripts.hasOwnProperty(url)) {
+      scripts[url] = {
+        loaded: false,
+        resolves: []
+      };
+      
+      return new Promise(function(resolve) { 
+        scripts[url].resolves.push(resolve);
+        if(/\.js$/.test(url)) {
+          var script = document.createElement('script');
+          script.type = 'text/javascript';
+          if(script.readyState) {
+            script.onreadystatechange = function() {
+              if(script.readyState === 'loaded' || script.readyState === 'complete') {
+                script.onreadystatechange = null;
+                resolveAll(url);
+              }
+            }
+          } else {
+            script.onload = function() { resolveAll(url); }
+          }
+          script.src = url;
+          document.getElementsByTagName('head')[0].appendChild(script);
+        } else if(/\.css$/.test(url)) {
+          var link = document.createElement('link');
+          link.type = 'text/css';
+          link.rel = 'stylesheet';
+          link.onload = function() { resolveAll(url); }
+          link.href = url;
+          document.getElementsByTagName('head')[0].appendChild(link);
+        } else {
+          // do nothing
+          resolveAll(url);
+        }
+      });
+      
+    } else {
+      // wait for the real promise to finish if already exists
+      return new Promise(function(resolve) {
+        if(!scripts[url].loaded) {
+          scripts[url].resolves.push(resolve);
+        } else {
+          // alread loaded
+          resolve();
+        }
+      });
+    }
+  };
+  
+  // resolve all script promises 
+  function resolveAll(url) {
+    if(scripts.hasOwnProperty(url) && !scripts[url].loaded) {
+      scripts[url].loaded = true;
+      scripts[url].resolves.forEach(function(resolve) { resolve(); });
+    }
+  }
+  
+  
+  // initialize App
+  // first load config and constants, then import module
+  function initApp() {    
+    loadConfig().then(loadMain).then();
+  }
+  // load configurations and utilities
+  // async
+  function loadConfig() {
+    return Promise.all([
+          app.load('lib/config/config.js'),
+          app.load('lib/config/strings.js'),
+          app.load('lib/util.js'),
+        ]);
+  }
+  
+  var loadingFlag = true;
+  // load main sources
+  // async
+  function loadMain() {
+    return Promise.all([
+      app.load('lib/data-loader.js').then(function() { app.model.init(); }),
+      app.load('lib/view-loader.js').then(function() { app.view.init(); }),
+      new Promise(function(resolve) {  // forced loading screen for no reason :P
+        setTimeout(swapMessage, 250);
+        setTimeout(function() { 
+          loadingFlag = false; 
+          resolve();
+        }, 3000+Math.random()*5000);
+      }),
+    ]).then(hideLoading);
+  }
+  
+  // wrapper function to hide loading screen
+  function hideLoading() {
+    app.view.hideLoading();
+  }
+  
+  // random message...
+  var messages = [
+    '不如搭巴士? How about bus?',
+    '你仲出街? Stay home!',
+    '望咩呀望? Nothing to see here.',
+    '搭巴士唔好咩? Why not bus?',
+    '走啦! Go away!',
+    '搭巴士啦! Take a bus!',
+    '啊~~~~~~ Ah~~~~~~~~',
+    '你個嘢壞咗呀! You are broken!',
+  ];
+  var currMsg = null;
+  function swapMessage() {
+    if(currMsg === null) {
+      currMsg = Math.floor(Math.random()*messages.length);
+    }
+    setTimeout(_swapMessage, 1000+Math.random()*1000);  
+  }
+  function _swapMessage() {
+    if(loadingFlag) {
+      document.getElementById('loading-msg').textContent = messages[currMsg];
+      currMsg = (currMsg+1)%messages.length;
+      swapMessage();
+    }
+  }
+  
+  initApp();
+  
+})(window.MTRDP);
